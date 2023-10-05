f8b360316d82f7b5686f958e653f7f3b0ffbe11c
Satsuki.U <ruri_ema@yahoo.co.jp>
2023-10-05 17:38:58 +0900

    ボタンの修正

--- resources/views/mypage/member/member_confirm.blade.php
+++ resources/views/mypage/member/member_confirm.blade.php
@@ -62,8 +62,8 @@
             </div>
 
             <div class="btns">
-            <button type="submit" name="action" value="submit" class="send-button">変更</button>
             <button type="submit" name="action" value="back" class="back-button">戻る</button>
+            <button type="submit" name="action" value="submit" class="send-button">変更</button>
             </div>
 
         </form>
